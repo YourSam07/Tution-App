ed449910afbb64e96ff9537f39b379728f0622a1
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2022-12-23 21:52:59 +0530

    floating btn

--- src/components/FloatingBtn.jsx
+++ src/components/FloatingBtn.jsx
@@ -8,17 +8,18 @@ const FloatingBtn = () => {
     <div>
       <div className="mainopShadow"></div>
       <div className="fab" onClick={() => setOpen(!open)}>
-        <div className={open ? "mainop grid place-items-center bg-white h-[175px]" : "mainop grid place-items-center bg-white"}>
+        {/* <div className={open ? "mainop grid place-items-center h-[175px]" : "mainop grid place-items-center bg-white"}> */}
+        <div className="mainop grid place-items-center bg-white">
           <img src={chat} alt="chat" className='w-8' />
         </div>
         <div id="sheets" className="minifab op2">
-        <a href="tel:+917276131102" target="_blank" rel="noreferrer" >
+          <a href="tel:+917276131102" target="_blank" rel="noreferrer" >
             <img className="minifabIcon" src={call} alt="call" />
           </a>
         </div>
         <div id="docs" className="minifab op1">
           <a href="https://wa.me/+919075957279" target="_blank" rel="noreferrer" >
-            <img className="minifabIcon" src={whatsapp} alt="whatsapp"/>
+            <img className="minifabIcon" src={whatsapp} alt="whatsapp" />
           </a>
         </div>
       </div>
